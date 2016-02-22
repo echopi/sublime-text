0602dbab1865c402ad191bc935651227edd27b25
echopi <eagleking0213@gmail.com>
2016-02-23 01:20:41 +0800

    sync ..

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 echo `date`
 cd /Users/jiewei.ljw/Sublime
 echo `date` >> git.log
-git diff origin/master --name-only >> git.log
+git diff --name-only >> git.log
 git add .
 git commit -m "sync .."
 git push
